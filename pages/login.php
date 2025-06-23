a2ea761552ff3f8610eb62f97b6fc4e00704cfea
Miguel Arismendi <meavthelast@gmail.com>
2025-06-23 08:41:15 -0400

    se vienen consitas (php)

---
+++ pages/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+        <link rel="stylesheet" href="../styles/style.css">
+        <title>Inicio de sesión</title>
+
+        <style>
+            button, .btn, input[type="button"], input[type="submit"], .hover-scale-up
+            {
+                transition: transform 0.3s ease-in-out;
+            }
+
+            button:hover, .btn:hover, input[type="button"]:hover, input[type="submit"]:hover, .hover-scale-up:hover
+            {
+                transform: scale(1.1);
+            }
+            .scale-down
+            {
+                height: auto;
+                width: 60%;
+            }
+        </style>
+    </head>
+
+    <body>
+        
+    
+        <script src="https://cdn.jsdelivr.net/npm/bo    otstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integ    rity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEM    VjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </body>
+</html>
