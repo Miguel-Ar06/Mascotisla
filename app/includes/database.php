12651851513a605022c3bfe851b935ab91bd2417
Miguel Arismendi <meavthelast@gmail.com>
2025-07-10 11:21:15 -0400

    Ya se conecta a la bdd para mostrar usuarios, falta insertarlos

--- app/includes/database.php
+++ app/includes/database.php
@@ -7,14 +7,14 @@ class Database
     public static $outputStatus;
     public static $connected = false;
     public static $result;
-
+    public static $executionSuccessful = false;
 
     public static function connect()
     {
         try 
         {
-            $pdo = new PDO('mysql:hostname=localhost;dbname=mascotisla;charset=utf8', 'root','');
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$pdo = new PDO('mysql:hostname=localhost;dbname=mascotisla;charset=utf8', 'root','');
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             self::$outputStatus = "successful connection";
             self::$connected = true;
@@ -32,13 +32,37 @@ class Database
         self::$pdo = null;
     }
 
-    // en caso de DELETE/INSERT/UPDATE devuelve el numero de filas afectadas
-    // en caso de SELECT devuelve un objeto representando una lista de todos los resultados;
+    // funcion para DELETE/INSERT/UPDATE: devuelve el numero de filas afectadas
     public static function execute($query)
     {
         if (!self::$connected)
         {
             self::$outputStatus = "failed execution: tried to run a query without a connection";
+            self::$executionSuccessful = false;
+            return;
+        }
+
+        try
+        {
+            self::$result = self::$pdo->exec($query);
+            self::$outputStatus = "Query successfully executed";
+            self::$executionSuccessful = true;
+
+        }
+        catch (PDOException $pdoException)
+        {
+            self::$outputStatus = "failed execution: "  . $pdoException->getMessage() . ' in ' . $pdoException->getFile() . ':' . $pdoException->getLine();
+
+        }
+    }
+
+    // funcion para los SELECT: devuelve un objeto representando una lista de todos los resultados;
+    public static function executeQuery($query)
+    {
+        if (!self::$connected)
+        {
+            self::$outputStatus = "failed execution: tried to run a query without a connection";
+            self::$executionSuccessful = false;
             return;
         }
 
@@ -46,8 +70,10 @@ class Database
         {
             // $result->fetch() para obtener o "leer" una fila, puede ser dentro de un while (da false si no hay filas)
             // ejemplo: $row = $result->fetch(); echo $row['usuarioId'] ; imprime el id del usuario leido
-            self::$result = self::$pdo->exec($query);
-            self::$outputStatus = "Query successfully executed";
+            self::$result = self::$pdo->query($query);
+            self::$outputStatus = "SELECT query successfully executed";
+            self::$executionSuccessful = true;
+
         }
         catch (PDOException $pdoException)
         {
--- app/includes/tablaColaboradores.php
+++ app/includes/tablaColaboradores.php
@@ -1,29 +1,40 @@
 <?php
-require __DIR__ . '/database.php';
 
-$testColaborators = [];
+$colaborators = [];
 Database::connect();
 
-$query = "SELECT miembros.id, cedula, colaboradores.nombre, apellido, correo, calle, referencia, ciudades.nombre, municipios.nombre
+$query = "SELECT miembros.id, cedula, colaboradores.nombre, apellido, correo, constrasena, calle, referencia, ciudades.nombre, municipios.nombre, esAdmin,
+            CASE
+                WHEN miembros.cedula_colaborador IS NOT NULL THEN TRUE
+                ELSE FALSE
+            END AS esMiembro
             FROM colaboradores
             LEFT JOIN miembros ON colaboradores.cedula = miembros.cedula_colaborador
             JOIN direcciones ON miembros.id_direccion = direcciones.id
             JOIN ciudades ON direcciones.id_ciudad = ciudades.id
             JOIN municipios ON ciudades.id_municipio = municipios.id;";
 
-Database::execute($query);
-
-while ($row = Database::$result->fetch())
+Database::executeQuery($query);
+$row;
+if (Database::$executionSuccessful)
 {
-    $newColaborator = new User
-    (
-        $row['miembros.id'],
-        $row['colaboradores.nombre'],
-        $row['apellido'],
-        $row['calle'] . '-' . $row['ciudades.nombre'] . '-' . $row['municipios.nombre'] . '-' . $row['referencia'],
-        $row['cedula'],
-        $row['correo'],
-    )
+    while ($row = Database::$result->fetch())
+    {
+        $newColaborator = new User
+        (
+            $row['miembros.id'],
+            $row['colaboradores.nombre'],
+            $row['apellido'],
+            $row['calle'] . '-' . $row['ciudades.nombre'] . '-' . $row['municipios.nombre'] . '-' . $row['referencia'],
+            $row['cedula'],
+            $row['correo'],
+            $row['contrasena'],
+            $row['esMiembro'],
+            $row['esAdmin']
+        );
+
+        $colaborators[] = $newColaborator;
+    }
 }
 
 // colaboradores de prueba para ir creando la plantilla generadora de tablas
@@ -36,7 +47,7 @@ $testColaborators =
 
 $editable = false;
 $deleteable = true;
-
+$num_columns = 8;
 function boolToSiONo($boolean)
 {
     if ($boolean == true)
@@ -67,30 +78,38 @@ function boolToSiONo($boolean)
             </tr>
         </thead>
         <tbody class="table-group-divider">
-            <?php foreach($testColaborators as $colaborator): ?>
+            <?php if (empty($colaborators)):?>
                 <tr>
-                    <th class="align-middle" scope="row"><?php echo $colaborator->getId() ?></th>
-                    <td class="align-middle"><?php echo $colaborator->getName() . ' ' . $colaborator->getLastName() ?></td>
-                    <td class="align-middle"><?php echo $colaborator->getIdentification() ?></td>
-                    <td class="align-middle"><?php echo $colaborator->getEmail() ?></td>
-                    <td class="align-middle"><?php boolToSiONo($colaborator->isMember()) ?></td>
-                    <td class="align-middle"><?php boolToSiONo($colaborator->isAdmin()) ?></td>
-                    <td>
-                        <form action="" method="GET">
-                            <input type="hidden" name="colaboratorId" value="<?php echo $colaborator->getId() ?>">
-                            <div><button type="submit" class="btn btn-primary hover-scale-up">Ver</button></div>
-                        </form>
+                    <td colspan="<?php echo $num_columns; ?>" class="text-center text-muted py-3 bg-light">
+                        No hay datos disponibles.
                     </td>
-                    <?php if($deleteable == true): ?>
-                    <td>
-                        <form action="" method="POST">
-                            <input type="hidden" name="colaboratorId" value="<?php echo $colaborator->getId() ?>">
-                            <div><button type="submit" class="btn btn-danger hover-scale-up">Eliminar</button></div>
-                        </form>
-                    </td>
-                    <?php endif ?>
                 </tr>
-            <?php endforeach ?>
+            <?php else: ?>
+                <?php foreach($colaborators as $colaborator): ?>
+                    <tr>
+                        <th class="align-middle" scope="row"><?php echo $colaborator->getId() ?></th>
+                        <td class="align-middle"><?php echo $colaborator->getName() . ' ' . $colaborator->getLastName() ?></td>
+                        <td class="align-middle"><?php echo $colaborator->getIdentification() ?></td>
+                        <td class="align-middle"><?php echo $colaborator->getEmail() ?></td>
+                        <td class="align-middle"><?php boolToSiONo($colaborator->isMember()) ?></td>
+                        <td class="align-middle"><?php boolToSiONo($colaborator->isAdmin()) ?></td>
+                        <td>
+                            <form action="" method="GET">
+                                <input type="hidden" name="colaboratorId" value="<?php echo $colaborator->getId() ?>">
+                                <div><button type="submit" class="btn btn-primary hover-scale-up">Ver</button></div>
+                            </form>
+                        </td>
+                        <?php if($deleteable == true): ?>
+                        <td>
+                            <form action="" method="POST">
+                                <input type="hidden" name="colaboratorId" value="<?php echo $colaborator->getId() ?>">
+                                <div><button type="submit" class="btn btn-danger hover-scale-up">Eliminar</button></div>
+                            </form>
+                        </td>
+                        <?php endif ?>
+                    </tr>
+                <?php endforeach ?>
+            <?php endif ?>
         </tbody>
     </table>
 </div>
